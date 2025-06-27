5e38392772f381f0e89b7e35eb0be0833779291d
Cril727 <criatiangarcia637@gmail.com>
2025-06-27 12:08:31 -0500

    Aprendiz Get

--- model/aprendizModel.ts
+++ model/aprendizModel.ts
@@ -1,1 +1,36 @@
+import { Conexion } from "./Conexion.ts";
 
+interface AprendizData {
+    idaprendiz:number | null;
+    nombre:string,
+    apellido:string,
+    email:string,
+    telefono:string
+}
+
+
+export class Aprendiz{
+    public _objAprendiz : AprendizData | null;
+
+    constructor(ObjAprendiz:AprendizData | null = null){
+        this._objAprendiz = ObjAprendiz
+    }
+
+    public async listarUsuarios():Promise<AprendizData[]>{
+        
+        try {
+            const result = await Conexion.execute("SELECT * FROM aprendiz")
+
+            if(!result || !result.rows){
+                console.warn("La consulta no devuelve datos");
+                return [];
+            }
+
+            return result.rows as AprendizData[];
+        } catch (error) {
+            console.error("Error al seleccionaer usuario"+error);
+            throw new Error("No se pueden obtener los datos")
+        }
+    }
+
+}
