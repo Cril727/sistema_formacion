90e22efcb3bebf74b154d8b448ba8a9dd7cab076
Cril727 <criatiangarcia637@gmail.com>
2025-06-27 11:20:45 -0500

    Aprendiz

--- controller/aprendizController.ts
+++ controller/aprendizController.ts
@@ -1,1 +1,7 @@
+import { Context } from "../dependencies/dependencies.ts";
 
+const aprendiz  = [{id:1,hh:"sdfs"}]
+
+export const getAprendiz = (ctx:Context)=>{
+    ctx.response.body = aprendiz
+}
--- routes/aprendizRoutes.ts
+++ routes/aprendizRoutes.ts
@@ -1,1 +1,13 @@
+import { Router } from "../dependencies/dependencies.ts";
+import { getAprendiz } from '../controller/aprendizController.ts';
 
+
+const AprendizRouter = new Router();
+
+AprendizRouter.get("/Aprendiz",getAprendiz)
+AprendizRouter.post("/Aprendiz",()=>{})
+AprendizRouter.put("/Aprendiz",()=>{})
+AprendizRouter.delete("/Aprendiz",()=>{})
+
+
+export{AprendizRouter}
