8ac7518b85020e99d2883141290d88d51bd6755a
Cril727 <criatiangarcia637@gmail.com>
2025-06-27 12:01:54 -0500

    Aprendiz Get

--- app.ts
+++ app.ts
@@ -1,7 +1,6 @@
 import { Application, oakCors } from "./dependencies/dependencies.ts";
 import { AprendizRouter } from "./routes/aprendizRoutes.ts";
 
-
 const app = new Application();
 
 app.use(oakCors())
@@ -12,6 +11,7 @@ routes.forEach(route=>{
     app.use(route.routes());
     app.use(route.allowedMethods())
 })
+
 
 console.log("Corriendo en el 8000")
 app.listen({port:8000})
--- controller/aprendizController.ts
+++ controller/aprendizController.ts
@@ -1,7 +1,46 @@
 import { Context } from "../dependencies/dependencies.ts";
+import { Aprendiz } from "../model/aprendizModel.ts";
 
-const aprendiz  = [{id:1,hh:"sdfs"}]
 
-export const getAprendiz = (ctx:Context)=>{
-    ctx.response.body = aprendiz
+export const getAprendiz = async (ctx:Context)=>{
+    const {response} = ctx;
+
+    try {
+
+        const objUsuario = new Aprendiz();
+        const ListaUsuarios = await objUsuario.listarUsuarios()
+
+        if(!ListaUsuarios || ListaUsuarios.length === 0){
+            response.status = 400;
+            response.body = {
+                success:false,
+                message:"No existen usuarios"
+            };
+            return;
+        };
+        
+
+        response.status = 200;
+        response.body = {
+            success:true,
+            data:ListaUsuarios
+        }
+
+    } catch (error) {
+        if(error instanceof Error){
+            response.status = 400;
+            response.body = {
+                success:false,
+                message:"Error del servidor",
+                error: error.message
+            }
+        }else{
+            response.status = 500;
+            response.body = {
+                success:false,
+                message:"Error interno del servidor",
+                error: String(error)
+            }
+        }
+    }
 }
