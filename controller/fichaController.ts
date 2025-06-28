920132b419770f439a6d54f53c9454a1e460db49
JC2405 <chaparrobarrerajulian@gmail.com>
2025-06-28 09:33:42 -0500

    agregar

--- controller/fichaController.ts
+++ controller/fichaController.ts
@@ -2,6 +2,18 @@ import { Context, z, RouterContext } from "../dependencies/dependencies.ts";
 import { Ficha } from "../model/fichaModel.ts";
 
 
+const FichaSchema = z.object({
+    codigo: z.string().min(1,"El nombre del codigo es obligatorio"),
+    fecha_inicio_lectiva: z.string().min(1,"La fecha de inicio lectiva es obligatoria"),
+    fecha_fin_lectiva: z.string().min(1,"La fecha de fin lectiva es obligatoria"),
+    fecha_fin_practica: z.string().min(1,"La fecha del fin de practicas es obligatoria"),
+    programa_idprograma: z.string().min(1)
+})
+
+
+
+
+
 export const getFicha = async (ctx:Context)=>{
     const {response} = ctx;
 
@@ -39,5 +51,64 @@ export const getFicha = async (ctx:Context)=>{
                 error: String(error)
             }
          }
+    }
+}
+
+export const postFicha = async (ctx:Context)=>{
+    const {request , response} = ctx;
+
+    try {
+        const contenLen = request.headers.get("Content-Length");
+
+        if(contenLen === "0"){
+            response.status = 400;
+            response.body = {
+                success:false,
+                message:"Cuerpo de la solicitud vacia"
+            };
+            return;
+        }
+
+        const body = await request.body.json();
+        const validated = FichaSchema.parse(body);
+
+        const fichaData = {
+        idficha: null,
+        ...validated,
+         programa_idprograma: Number(validated.programa_idprograma)
+        };
+
+        const objFicha = new Ficha(fichaData)
+        const agregar = await objFicha.agregarFicha();
+
+        if (agregar.success) {
+            response.status = 200;
+            response.body = {
+                success:true,
+                message:"Ficha agregada correctamente",
+                ficha:agregar.ficha
+            }
+        }else{
+            response.status = 400;
+            response.body = {
+                success:false,
+                message:agregar.message
+            }
+        }
+    } catch (error) {
+         if(error instanceof z.ZodError){
+            response.status = 400;
+            response.body = {
+                success: false,
+                message:"Datos invalios",
+                errors: error.format(),
+            }
+        }else{
+            response.status = 500;
+            response.body = {
+                success:false,
+                message:"Datos invalidos",
+            }
+        }
     }
 }
--- model/fichaModel.ts
+++ model/fichaModel.ts
@@ -36,4 +36,48 @@ export class Ficha {
             
         }
     }
+    
+    public async agregarFicha(): Promise<{success:boolean, message: string, ficha?: Record<string,unknown> }>{
+        try {
+            if (!this._objFicha) {
+                throw new Error("Objeto no valido")
+            }
+
+            const { codigo,fecha_inicio_lectiva,fecha_fin_lectiva,fecha_fin_practica,programa_idprograma } = this._objFicha;
+
+            console.log("Objeto recibido en agregarFicha:", this._objFicha);
+            if (!codigo || !fecha_inicio_lectiva || !fecha_fin_lectiva || !fecha_fin_practica || !programa_idprograma ==null) {
+                    throw new Error("Faltan campos requeridos")
+            }
+
+            await Conexion.execute("START TRANSACTION");
+            const sql = await Conexion.execute("INSERT INTO ficha(codigo,fecha_inicio_lectiva,fecha_fin_lectiva,fecha_fin_practica,programa_idprograma) values(?,?,?,?,?)",
+                [codigo,fecha_inicio_lectiva,fecha_fin_lectiva,fecha_fin_practica,programa_idprograma],
+            );
+
+            if (sql && typeof sql.affectedRows === "number" && sql.affectedRows>0) {
+                const[ficha] = await Conexion.query("SELECT * FROM ficha WHERE idficha = LAST_INSERT_ID()",)
+
+                await Conexion.execute("COMMIT");
+
+                return {
+                    success: true,
+                    message: "Ficha Agregado Correctamente",
+                    ficha: ficha    
+                };
+                
+            } else {
+                throw new Error('Error al agregar la ficha')
+            }
+        } catch (error) {
+              await Conexion.execute("ROLLBACK");
+            if (error instanceof Error) {
+                return { success: false, message: error.message };
+            } else {
+                return { success: false, message: "error interno del servidor" };
+            }
+        }
+    }
+
+   
 }
