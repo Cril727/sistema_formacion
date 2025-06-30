d306f6a24e81280eb2895d88aaecf32f6f26637e
Cril727 <criatiangarcia637@gmail.com>
2025-06-29 21:19:16 -0500

    agregar profesiones

--- controller/profesionController.ts
+++ controller/profesionController.ts
@@ -1,6 +1,10 @@
 import { Context,z,RouterContext } from "../dependencies/dependencies.ts";
 import { Profesion } from "../model/profesionModel.ts";
 
+const programaSchema = z.object({
+    nombre_profesion : z.string().min(1,"El nombre de la profesion es requerida")
+})
+
 
 export const getProfesion = async (ctx:Context)=>{
 
@@ -47,6 +51,62 @@ export const getProfesion = async (ctx:Context)=>{
 
 
 export const postProfesion = async (ctx:Context)=>{
+
+    const {request, response} = ctx
+
+    try {
+        const contentLen = request.headers.get("ContenetLengh")
+
+        if(contentLen === "0"){
+            response.status = 400;
+            response.body = {
+                success:false,
+                message:"Cuerpo de la solicitud vacia"
+            }
+            return;
+        }
+
+        const body = await request.body.json();
+        const validated = programaSchema.parse(body);
+
+        const profesionData = {
+            idprofesion:null,
+            ...validated
+        }
+
+        const objProfesion = new Profesion(profesionData);
+        const agregar = await objProfesion.agregarProfesion()
+
+        if(agregar.success){
+            response.status = 201;
+            response.body = {
+                success: true,
+                message: "Profesion agregada correctamente",
+                profesion: agregar.profesion
+            }
+        }else{
+            response.status = 400;
+            response.body = {
+                success:false,
+                message: agregar.message
+            }
+        }
+    } catch (error) {
+        if(error instanceof z.ZodError){
+            response.status = 400;
+            response.body = {
+                success: false,
+                message:"Datos invalidos",
+                errors: error.format()
+            }
+        }else{
+            response.status = 500;
+            response.body = {
+                success:false,
+                message:"Error del servidor",
+            }
+        }
+    }
     
 }
 
--- model/profesionModel.ts
+++ model/profesionModel.ts
@@ -29,4 +29,46 @@ export class Profesion{
             throw new Error("Error al obtener las profesiones")
         }
     }
+
+
+    public async agregarProfesion():Promise<{success:boolean, message:string, profesion?: Record <string, unknown>}>{
+        try {
+
+            if(!this._objProfesion){
+                throw new Error("Objeto no valido")
+            }
+
+            const { nombre_profesion } = this._objProfesion;
+
+            await Conexion.execute("START TRANSACTION")
+            const agregar = await Conexion.execute("INSERT INTO profesion(nombre_profesion) VALUES (?)", [nombre_profesion],)
+
+            if(agregar && typeof agregar.affectedRows === "number" && agregar.affectedRows>0){
+                const [profesion] = await Conexion.query("SELECT * FROM profesion WHERE idprofesion = LAST_INSERT_ID()",);
+
+                await Conexion.execute("COMMIT");
+
+                return {
+                    success:true,
+                    message:"Pofesion agregada correctamente",
+                    profesion: profesion
+                }
+            }else{
+                throw new Error("Error al agregar la profesion")
+            }
+        } catch (error) {
+            await Conexion.execute("ROLLBACK")
+            if(error instanceof Error){
+                return{
+                    success:false,
+                    message: error.message
+                }
+            }else{
+                return{
+                    success:false,
+                    message:"Error de servidor"
+                }
+            }
+        }
+    }
 }
