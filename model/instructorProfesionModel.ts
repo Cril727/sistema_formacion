332d3bc951309ce91578427a6cb4ef33a2cc9f57
Cril727 <criatiangarcia637@gmail.com>
2025-06-30 12:58:40 -0500

    agregar relacion entre instructor y profesion

--- model/instructorProfesionModel.ts
+++ model/instructorProfesionModel.ts
@@ -1,11 +1,11 @@
 import { Conexion } from "./Conexion.ts";
 
 interface InstructorProfesionData {
-  instructor_id: number | null;
+  instructor_idinstructor: number | null;
   instructor_nombre: string;
   instructor_apellido: string;
   instructor_email: string;
-  profesion_id: number | null;
+  profesion_idprofesion: number | null;
   profesion_nombre: string;
 }
 
@@ -31,6 +31,54 @@ export class InstructorProfesion {
     } catch (error) {
         console.log("Error al mostrar los datos" + error);
         throw new Error("Error del servidor no se pueden optener los datos")
+    }
+  }
+
+
+  public async agregarInstructorProfesion():Promise<{success:boolean, message:string, instructorProfesion?:Record<string,unknown>}>{
+    try {
+        if(!this._objInstructorProfesion){
+            throw new Error("Objeto invalido")
+        }
+
+        const {instructor_idinstructor,profesion_idprofesion} = this._objInstructorProfesion;
+
+        await Conexion.execute("START TRANSACTION");
+        const agregar = await Conexion.execute("INSERT INTO instructor_has_profesion (instructor_idinstructor, profesion_idprofesion) VALUES (?, ?)",
+            [
+                instructor_idinstructor,
+                profesion_idprofesion
+            ],
+        );
+
+        if(agregar && typeof agregar.affectedRows === "number" && agregar.affectedRows >0){
+            const [instructorProfesion] = await Conexion.query("SELECT i.idinstructor AS instructor_idinstructor, i.nombre AS instructor_nombre, i.apellido AS instructor_apellido, i.email AS instructor_email, p.idprofesion AS profesion_idprofesion, p.nombre_profesion AS profesion_nombre FROM instructor_has_profesion ip JOIN instructor i ON ip.instructor_idinstructor = i.idinstructor JOIN profesion p ON ip.profesion_idprofesion = p.idprofesion WHERE ip.instructor_idinstructor = ? AND ip.profesion_idprofesion = ? LIMIT 1",
+                [instructor_idinstructor, profesion_idprofesion],
+            )
+
+            await Conexion.execute("COMMIT");
+
+            return{
+                success:true,
+                message:"Se ha guardado correctamente la realcion entre instructor y profesion",
+                instructorProfesion:instructorProfesion
+            }
+        }else{
+            throw new Error("Error al crear la relacion entre el instructor y la profesion")
+        }
+
+    } catch (error) {
+        if(error instanceof Error){
+            return{
+                success:false,
+                message:error.message
+            }
+        }else{
+            return{
+                success:false,
+                message:"Error del servidor"
+            }
+        }
     }
   }
 }
--- routes/instructorProfesionRouter.ts
+++ routes/instructorProfesionRouter.ts
@@ -1,11 +1,11 @@
-import { getInstructorProfesion } from "../controller/instructorProfesionController.ts";
+import { getInstructorProfesion, postInstructoresProfesiones } from "../controller/instructorProfesionController.ts";
 import { Router } from "../dependencies/dependencies.ts";
 
 
 const InstructorProfesionRouter = new Router();
 
 InstructorProfesionRouter.get("/InstructoresProfesiones",getInstructorProfesion);
-InstructorProfesionRouter.post("/InstructorProfesion",()=>{});
+InstructorProfesionRouter.post("/InstructorProfesion",postInstructoresProfesiones);
 InstructorProfesionRouter.put("/InstructorProfesion",()=>{});
 InstructorProfesionRouter.delete("/InstructorProfesion",()=>{});
 
